7de3961713ef1aad21ff2fb5c4a2fe1aa99df816
raihanph <raihan.kabir.berlin@gmail.com>
2025-05-03 08:54:02 +0600

    next commint

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <h1 className='text-4xl'> Hello Yeasrib..</h1>
+      <h1 className='text-center text-5xl font-bold mt-8 mb-2'> Hello Yeasrib..</h1>
+      <h3 className=' px-4 py-40  bg-blue-300 mx-10 text-center items-center text-red-500 text-6xl'> Raihan..</h3>
     </div>
   )
 }
